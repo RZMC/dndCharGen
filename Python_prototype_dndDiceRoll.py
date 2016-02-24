57329ff8cfeae72841e27a9b6e77e98b94c4acec
RZMC <rzm.cascadia@gmail.com>
2016-02-23 17:04:44 -0800

    didn't feel like I got much done
    Error checking
    making sure it wouldn't shit itself if somethign was missing
    trimmign the code a bit
    jsut tryign to make it look better and actually work mostly
    
    you can do stat rolls and look them up without changing them now

--- Python_prototype_dndDiceRoll.py
+++ Python_prototype_dndDiceRoll.py
@@ -1,125 +1,144 @@
 '''imports the command for generating random numbers'''
 from random import randint
-
 print()
 print("==========Program Start==========")
-print()
-
-print()
-print("==========Checking Modules==========")
-print()
-
+"""Rule and table placeholder base"""
+class nomod(object):
+	ruleCheck=False
+	def n(self):
+		print()
+		return print("No %s Module Loaded" % str(self.checkName))
 """Rule and Table Placeholders"""
-class attributeRolls(object):
-	ruleCheck=False
+class attributeRolls(nomod):
 	def __init__(self):
-		self.checkName="Dice"
-class raceRules(object):
-	ruleCheck=False
+		self.checkName="Dice Module"
+	def moduleCheck(self):
+		return self.n()
+	def statRolls(self):
+		return self.n()
+	def displayStatResults(self):
+		return self.n()
+dice = attributeRolls()
+class raceRules(nomod):
 	def __init__(self):
-		self.checkName="Race"
-
-'''config file check that handles imports goes here'''
-#from pp_module_diceRule import attributeRolls
+		self.checkName="Race Module"
+race = raceRules()
+print("==========Checking Modules==========")
+'''config file check that handles imports goes here, until them import will just go here'''
 from pp_module_diceRule import dice
-#'''importing like in the following line does not work except for singular instances, just for reference'''
+#from pp_module_raceRule import race
+#print()
+#dice.moduleCheck()
+'''importing like in the following line does not work except for singular instances, just for reference'''
 #import pp_module_diceRule
-
-dice.statRolls()
-
-print()
-
-
-
 '''rule check method goes here, referenced later for checks'''
 def rulecheck(s):
 	if s.ruleCheck == True:
 		ruleCheck = True
-		print("%s Rules Status =" % s.checkName, s.ruleCheck)
-		return print("%s Rules Status: Present" % s.checkName)
+		return print("%s Rules: Present and %s." % (s.checkName, s.ruleCheck))
 	elif s.ruleCheck == False:
-		print("%s Rules Status =" % s.checkName, s.ruleCheck)
-		return print("%s Rules Status: Not Present please select" % s.checkName)
-
-
-
-'''welcome message'''		
-print("==========Welcome to the prototype==========")
-
-
+		return print("%s Rules: Not Present and %s, please select" % (s.checkName, s.ruleCheck))
 print("")
 """Rule Checks"""
 """Referenced Immediately and as option 5 of overmenu"""
 """checks to make sure rule has been loaded by checking 'ruleCheck' variable in each class using 'rulecheck' method created earlier in the prototype main file"""
 def ruleCheckList():
 	rulecheck(dice)
-	rulecheck(raceRules())
+	rulecheck(race)
 	return
-
 ruleCheckList()
-print("")
-
-
-#overmenu function
-def overmenu():
-	print("Please make a selection")
-	print("(1) Character Generation")
-	print("(2) Utilities")
-	print("(3) Options")
-	print("(4) Quit Program")
-
-	menu1 =input()
-
-	if menu1 == "1":
-		print("You selected %s Character Generation" % menu1)
-		#character Generation
-		#needs several options
-		#option-1 is appearance
-		#option-2 is dice rolls
-		charGenMenu = input()
-
-
-		pass
-	elif menu1 == "2":
-		print("You selected %s Utilities" % menu1)
-		#Utilities
-		pass
-	elif menu1 == "3":
-		print("You selected %s Options" % menu1)
-		#Options
-		pass
-	elif menu1 == "4":
-		print("You selected %s Exit Program" % menu1)
+def charmenu():
+	print()
+	print("==========Character Generation==========")
+	print()
+	print("(1) Display Current Character Information")
+	print("(2) Roll for Attributes")
+	print("(t) Main Menu")
+	print("(q) Quit Program")
+	#dice.statRolls()
+	charMenu = input("Please enter an option:")
+	if charMenu == "t":
+		return mainmenu()
+	elif charMenu == "1":
+		dice.displayStatResults()
+		return charmenu()
+	elif charMenu == "2":
+		dice.statRolls()
+		return charmenu()
+	elif charMenu == "q":
 		return quit()
-		#Exit Program
-	elif menu1 == "5":
-		print("You selected %s Check Imports" % menu1)
-		print()
-		ruleCheckList()
-		print()
-		return overmenu()
-		#Exit Program
 	else:
 		print()
 		print("Invalid selection please try again")
+		return charmenu()
+	#character Generation
+	#needs several options
+	#option-1 is appearance
+	#option-2 is dice rolls
+def utilitymenu():
+	print()
+	print("==========Utilities==========")
+	print()
+	print("(t) Main Menu")
+	print("(q) Quit Program")
+	#dice.statRolls()
+	utilMenu = input("Please enter an option:")
+	if utilMenu == "t":
+		return mainmenu()
+	elif utilMenu == "q":
+		return quit()
+	else:
 		print()
-		return overmenu()
-
-def charGen_diceRollBasic():
-	#this is where dice rolls and the handling of said dice rolls goes
-	#needs 6 sets of 4d6 rolls
-	#needs to drop the lowest for each set
-	#needs to output each set to a list of three rolls within a list of 6 sets
-	#needs Mulligan option where all 6 sets are rerolled but can only be run one time
-	pass
-
+		print("Invalid selection please try again")
+		return utilitymenu()
+def optionsmenu():
+	print()
+	print("==========Options==========")
+	print()
+	print("(t) Main Menu")
+	print("(q) Quit Program")
+	#dice.statRolls()
+	optionMenu = input("Please enter an option:")
+	if optionMenu == "t":
+		return mainmenu()
+	elif optionMenu == "q":
+		return quit()
+	else:
+		print()
+		print("Invalid selection please try again")
+		return optionsmenu()
+def mainmenu():
+	print()
+	print("==========Main Menu==========")
+	print()
+	print("(1) Character Generation")
+	print("(2) Utilities")
+	print("(3) Options")
+	print("(4) check imports")
+	print("(q) Quit Program")
+	menuTop =input("Please enter an option:")
+	if menuTop =="t":
+		return mainmenu()
+	elif menuTop == "1":
+		return charmenu()
+	elif menuTop == "2":
+		return utilitymenu()
+	elif menuTop == "3":
+		return optionsmenu()
+	elif menuTop == "4":
+		print("You selected %s Check Imports" % menuTop)
+		print()
+		ruleCheckList()
+		return mainmenu()
+	elif menuTop == "q":
+		return quit()
+	else:
+		print()
+		print("Invalid selection please try again")
+		return mainmenu()
 def quit():
+	print()
+	print("Quitting Program")
 	input("Press Enter to close the program")
-
-overmenu()
-
+mainmenu()
 input("Nothing left to run, press Enter to close the program")
-
-
-
-
--- pp_module_diceRule.py
+++ pp_module_diceRule.py
@@ -1,37 +1,65 @@
 from random import randint
 '''Custom Variables, this is where you adjust the module for personal use'''
-
-die_sides = 6
+number_of_sides_on_die = 6
 die_number_per_set = 4
 sets_of_dice_rolls = 6
-
-moduleName = "Dice Module (with flavor text)"
-
-'''Rules for attribute rolls, Do not alter anythign past this line'''
+#reroll Die =
+#Drop Lowest =
+moduleName = "Dice Module"
+#moduleName = "The Lusty Orc Dice Module"
+'''Rules for attribute rolls, Do not alter anything past this line'''
 class attributeRolls(object):
 	'''sets the rule toggle to True so that the system will know that the rule has been loaded'''
 	ruleCheck=True
-	def __init__(self, die_number, die_sides):
+	def __init__(self, die_sides, die_number, die_sets):
 		self.checkName = moduleName
-		self.number = die_number_per_set
+		"""sets the module name"""
 		self.dSides = die_sides
-		self.sets = sets_of_dice_rolls
+		"""sets the number of sides on a die"""
+		self.dSetNumber = die_number
+		"""sets the number of dice rolled per die set"""
+		self.dSets = die_sets
+		"""sets the number of totl die sets rolled"""
+		self.rollSets = ["", "No dice have been rolled"]
+		"""set's the self.rollSets list to empty so the program does not freak out if statRollsDisplay is called"""
+	def moduleCheck(self):
+		print("//run module Check start//")
+		print("number of sides on each die = %s" % self.dSides)
+		print("number of die in a set= %s" % self.dSetNumber)
+		print("number of die sets= %s" % self.dSets)
+		print("//run module Check complete//")
 	def statRolls(self):
-		print("2 placeholder")
-		sets = []
-		for s in range(self.sets):
+		"""this is the method that is called to roll attribute values for a character for the first time, DND3.5 default is 6 sets of 4 rolls of 6 sided dice, however this is designed so that can be adjusted"""
+		rollSets = []
+		"""creats the Top Set Container that will house all of the sets or if it exists zeroes out the values"""
+		for s in range(self.dSets):
+			"""creates Recursion loop that ends after a number of times equal to sets_of_dice_rolls variable"""
 			rolls = []
-			for r in range(self.number):
+			"""while the rollSets recursion loop is running a roll container is set to empty so that values can be added to it"""
+			for r in range(self.dSetNumber):
+				"""creates another recursion loop that runs for as many times as die_number_per_set"""
 				rolls.append(randint(1, self.dSides))
-			sets.append(rolls)
-		print(sets)
-		pass
-		#list with 6 sets
+				"""adds to theresult of a roll that starts at 1 and ends at the value of number_of_sides_on_die to the rolls set container"""
+			rollSets.append(rolls)
+			"""after the rolls are done, normally 4 of them, the set is added to the rollSets variable container"""
+		print("Stat Rolls")
+		"""after all of the sets have been added to the set container the sets are printed out, the following 3 lines make it print the sets vertically"""
+		for rs in range(len(rollSets)):
+			self.rollSets = rollSets
+			print(self.rollSets[rs])
+	def displayStatResults(self):
+		"""This method when called will print the results of the rollSets if they exist"""
+		print("Stat Rolls")
+		for rs in range(len(self.rollSets)):
+			print(self.rollSets[rs])
+		return
 		#each set contains the three highest rolls after 4 rolls
 		#should end up with6 sets of 3 dice rolls
-		#may as well jsut add them and produce the total to save on list coding but still print the results of the rolls
-dice = attributeRolls(die_number_per_set, die_sides)
-#print("%s imported" % moduleName)
-#print("%s check = %s" % (moduleName, attributeRolls.ruleCheck))
-print("sides = %s" % dice.number)
-print("faces = %s" % dice.dSides)
+		#may as well just add them and produce the total to save on list coding but still print the results of the rolls
+dice = attributeRolls(number_of_sides_on_die, die_number_per_set, sets_of_dice_rolls)
+
+
+
+"""statRolls and displayStatRolls still needs dynamic reroll for lowest or threshhold, drop lowest and totaling that will print out next to the set rolls like "[3, 4, 3, 4] 1" or something, maybe attribute modifiers too"""
+"""Also Manual input with a statement that says they were manually input"""
+"""probably number of times rolled statement too for mulligans until I can figure out some good mulligan rules"""
