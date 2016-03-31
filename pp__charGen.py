4008e78a5358615d7e294583420826bf77c12b75
RZMC <rzm.cascadia@gmail.com>
2016-03-31 15:55:51 -0700

    Added lowest attribute and modifier averages, cleaned ccode
    #Added option for minimum possible Attribute average
    #Added option for minimum possible modifier average
    #realized i had wasted space with shit code in the attribute modifier

--- pp__charGen.py
+++ pp__charGen.py
@@ -63,18 +63,18 @@ def charmenu(cReadout):
 	print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n==========Character Generation==========\n")
 	#readout for char
 	if cReadout==999:print(invalid)
-	elif cReadout==1:dice.displayStatResults()
+	elif cReadout==1:dice.displayCurrent()
 		#cReadout 1 needs to be expanded to display all current character information
-	elif cReadout==2:dice.statRolls()
+	elif cReadout==2:return attributeMenu(0)
 	elif cReadout==11:ruleCheckList()
 	elif cReadout==12:print(dice.moduleCheck())
 	else:print("")
 	#options list, it references debug options
-	print("\n[1] Display Current Character Information\n%s%s[2] Roll for Attributes\n[/] or [t] Main Menu\n[*] or [q] Quit Program" % (cDebug1, cDebug2))
+	print("\n[1] Display Current Character Information\n%s%s[2] Attribute Managment\n[/] or [t] Main Menu\n[*] or [q] Quit Program" % (cDebug1, cDebug2))
 	#CharMenu input
 	menuSel=raw_input(plsEn)
 	if menuSel in("t","/"):return mainmenu(0)
-	elif menuSel=="1": return charmenu(1)
+	elif menuSel=="1":return charmenu(1)
 	elif menuSel=="2":return charmenu(2)
 	elif menuSel in ("*", "q"):return quit()
 	#CharMenu Debug Options
@@ -88,48 +88,68 @@ def charmenu(cReadout):
 	#-should use dictionary for each entry
 	#-entry zero should have the descriptor names like name, weight, description
 
-
-
-
-def utilitymenu(uReadout):
-	"""the following line checks the global debug variable so it doesn't freak out for usign a variable not defined in the function"""
-	global debug
+def attributeMenu(attributeReadout):
 	#header
-	print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n==========Utilities==========\n")
-	#readout for utility
-	if uReadout==999:print(invalid)
+	print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n==========Attribute Menu==========\n")
+	#readout for dice
+	if attributeReadout==999:print(invalid)
+	elif attributeReadout==1:dice.displayStatResults()
+	elif attributeReadout==2:dice.statRolls()
 	else:print("")
-	#debug status
-	if debug:print("\nDebug Options: Active\n")
-	else:print("\nDebug Options: Inactive\n")
-	#options list
-	print("[1] Toggle Debug\n[/] or [t] Main Menu\n[*] or [q] Quit Program")
-	#menu input for utility
+	#options list, it references debug options
+	print("\n[1] Display Current Dice Rolls Information\n[2] Roll for Attributes\n[0] or [r] Character Generation\n[/] or [t] Main Menu\n[*] or [q] Quit Program")
+	#attributeMenu input
 	menuSel=raw_input(plsEn)
 	if menuSel in("t","/"):return mainmenu(0)
-	elif menuSel=="1":
-		if debug:debug=False
-		else:debug=True
-		return utilitymenu(0)
+	elif menuSel=="1": return attributeMenu(1)
+	elif menuSel=="2":return attributeMenu(2)
+	elif menuSel in ("0", "r"):return charmenu(0)
 	elif menuSel in ("*", "q"):return quit()
-	else:return utilitymenu(999)
+	#Return invalid entry
+	else:return attributeMenu(999)
 
 
 
 
 def optionsmenu(oReadout):
+	"""the following line checks the global debug variable so it doesn't freak out for usign a variable not defined in the function"""
+	global debug
 	#header
 	print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n==========Options==========\n")
 	#readout for options
 	if oReadout==999:print(invalid)
 	else:print("")
+	#debug status
+	if debug:print("\nDebug Options: Active\n")
+	else:print("\nDebug Options: Inactive\n")
 	#Options list
-	print("\n[/] or [t] Main Menu\n[*] or [q] Quit Program")
+	print("[1] Toggle Debug\n[/] or [t] Main Menu\n[*] or [q] Quit Program")
 	#menu input for options
 	menuSel=raw_input(plsEn)
 	if menuSel in("t","/"):return mainmenu(0)
+	elif menuSel=="1":
+		if debug:debug=False
+		else:debug=True
+		return optionsmenu(0)
 	elif menuSel in ("*", "q"):return quit()
 	else:return optionsmenu(999)
+
+
+
+
+def utilitymenu(uReadout):
+	#header
+	print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n==========Utilities==========\n")
+	#readout for utility
+	if uReadout==999:print(invalid)
+	else:print("")
+	#options list
+	print("\n[/] or [t] Main Menu\n[*] or [q] Quit Program")
+	#menu input for utility
+	menuSel=raw_input(plsEn)
+	if menuSel in("t","/"):return mainmenu(0)
+	elif menuSel in ("*", "q"):return quit()
+	else:return utilitymenu(999)
 
 
 
@@ -146,13 +166,13 @@ def mainmenu(mReadout):
 	elif mReadout==991:print("%s\n%s" % (ruleCheckList(), dice.moduleCheck()))
 	else:print("")
 	#Options list, it references debug options
-	print("\n[1] Character Generation\n%s[2] Utilities\n[3] Options\n[*] or [q] Quit Program" % (mDebug1))
+	print("\n[1] Character Generation\n%s[2] Options\n[3] Utilities\n[*] or [q] Quit Program" % (mDebug1))
 	#menu input for main
 	menuSel=raw_input(plsEn)
 	if menuSel in("t","/"):return mainmenu(0)
 	elif menuSel=="1":return charmenu(0)
-	elif menuSel=="2":return utilitymenu(0)
-	elif menuSel=="3":return optionsmenu(0)
+	elif menuSel=="2":return optionsmenu(0)
+	elif menuSel=="3":return utilitymenu(0)
 	#mainmenu debug inputs
 	elif menuSel=="11" and debug:return mainmenu(11)
 	elif menuSel in ("*", "q"):return quit()
@@ -199,3 +219,4 @@ raw_input("Nothing left to run, press Enter to close the program")
 """importing info"""
 #importing as in the following line does not work except for singular instances, just for reference
 #import pp_module_diceRule
+
--- pp_module_diceRule.py
+++ pp_module_diceRule.py
@@ -1,5 +1,4 @@
-#this file is explicitly for rolling dice
-from random import randint
+
 '''Custom Variables, this is where you adjust the module for personal use'''
 the_lowest_possible_roll=1
 the_number_of_sides_on_a_die=6
@@ -8,17 +7,23 @@ the_number_of_sets_of_dice_rolls=6
 reroll_if_equal_or_less=0
 number_of_lowest_rolls_to_drop_in_a_set=1
 number_of_highest_rolls_to_drop_in_a_set=0
-
+minimum_average_attribute_or_else_reroll=3
+#raising minimum_average_attribute_modifier_or_else_reroll above 2.5 is not recomended with Dungeons and Dragons stat rolls and with default recursion limit will crash the program
+minimum_average_attribute_modifier_or_else_reroll=-1
 
 #mulligan_yes_or_no=True not implemented
 moduleName="Dice Module"
 #moduleName="The Lusty Orc Bride Dice Module"
-#Rules for attribute rolls, Do not alter anything past this line
+"""Rules for attribute rolls, Do not alter anything past this line"""
+#imports the command for generating random numbers
+from random import randint
+#import sys
+#sys.setrecursionlimit(10000)
 class attributeRolls(object):
 	#sets the rule toggle to True so that the program will know that the rule has been loaded
 	ruleCheck=True
 	#sets the module name, the number of die sides, number rolled per set, total die sets rolled and the lists to empty so the program does not freak out and then sets the set batch number to 0
-	def __init__(self, lowest_roll, die_sides, die_number, die_sets, reroll_die, low_drop, high_drop):
+	def __init__(self, lowest_roll, die_sides, die_number, die_sets, reroll_die, low_drop, high_drop, att_avg, mod_avg):
 		self.moduleName=moduleName
 		self.the_lowest_possible_roll=lowest_roll
 		self.the_number_of_sides_on_a_die=die_sides
@@ -27,14 +32,18 @@ class attributeRolls(object):
 		self.reroll_if_equal_or_less=reroll_die
 		self.number_of_lowest_rolls_to_drop_in_a_set=low_drop
 		self.number_of_highest_rolls_to_drop_in_a_set=high_drop
-		self.list_of_roll_set_results=["No dice have been rolled"]
-		self.list_of_roll_set_results_adjusted=["No dice have been rolled"]
+		self.minimum_average_attribute_or_else_reroll=att_avg
+		self.minimum_average_attribute_modifier_or_else_reroll=mod_avg
+		self.attribute_average=0
+		self.attribute_modifier_average=0
+		self.list_of_roll_set_results=[""]
+		self.list_of_roll_set_results_adjusted=[""]
 		self.attribute_results=["No dice have been rolled"]
 		self.attribute_results_modifier=[""]
 		self.batch=0
 
-	def moduleCheck(self):return ("//run module Check start//\nlowest possible roll =%s\nhighest possible roll = %s\nnumber of die rolled in a set = %s\nnumber of die sets rolled = %s\nreroll if less than or equal to = %s\nnumber of lowest rolls in a set to drop =%s\nnumber of highest rolls in a set to drop = %s\n//run module Check complete//" % (self.the_lowest_possible_roll, self.the_number_of_sides_on_a_die, self.the_number_of_rolls_in_a_set, self.the_number_of_sets_of_dice_rolls, self.reroll_if_equal_or_less, self.number_of_lowest_rolls_to_drop_in_a_set, self.number_of_highest_rolls_to_drop_in_a_set))
-	
+	def moduleCheck(self):return ("//run module Check start//\nlowest possible roll =%s\nhighest possible roll = %s\nnumber of die rolled in a set = %s\nnumber of die sets rolled = %s\nreroll if less than or equal to = %s\nnumber of lowest rolls in a set to drop =%s\nnumber of highest rolls in a set to drop = %s\nlowest possible modifier average =%s\n//run module Check complete//" % (self.the_lowest_possible_roll, self.the_number_of_sides_on_a_die, self.the_number_of_rolls_in_a_set, self.the_number_of_sets_of_dice_rolls, self.reroll_if_equal_or_less, self.number_of_lowest_rolls_to_drop_in_a_set, self.number_of_highest_rolls_to_drop_in_a_set, self.minimum_average_attribute_modifier_or_else_reroll))
+
 	def roll_set_of_dice(self):
 		self.set_of_dice_rolls=[]
 		self.set_of_dice_rolls_adjusted=[]
@@ -66,45 +75,49 @@ class attributeRolls(object):
 		self.list_of_roll_set_results_adjusted=[]
 		self.attribute_results=[]
 		self.attribute_results_modifier=[]
+		self.attribute_average=0
+		self.attribute_modifier_average=0
 		for set_count_number in range(self.the_number_of_sets_of_dice_rolls):
 			self.roll_set_of_dice()
 			self.list_of_roll_set_results.append(self.set_of_dice_rolls)
 			self.list_of_roll_set_results_adjusted.append(self.set_of_dice_rolls_adjusted)
-		self.batch+=1
-		print("number of batch attempts:"+str(self.batch)+"\nStat Rolls")
 		for batchSets in range(len(self.list_of_roll_set_results)): 
 			at=0
 			at_mod=0
 			for batchRolls in range(len(self.list_of_roll_set_results_adjusted[batchSets])):at+=self.list_of_roll_set_results_adjusted[batchSets][batchRolls]
 			self.attribute_results.append(at)
+			self.attribute_average+=at
 			at-=10
-			if at<0:
-				if at % 2 == 0:
-					at/=2
-					at_mod=at
-				else:
-					at-=1
-					at/=2
-					at_mod=at
-			elif at>0:
-				if at % 2 == 0:
-					at/=2
-					at_mod=at
-				else:
-					at-=1
-					at/=2
-					at_mod=at
-			else: at_mod=at
+			if at==0:at_mod=at
+			elif at % 2 == 0: at_mod=at/2
+			elif at % 2 == 1:at_mod=(at-1)/2
+			else:return dice.statRolls()
 			self.attribute_results_modifier.append(int(at_mod))
-			print((self.list_of_roll_set_results[batchSets]), (self.attribute_results[batchSets]), self.attribute_results_modifier[batchSets])
-		self.displayStatResults()
+			self.attribute_modifier_average+=at_mod
+		self.attribute_average/=self.the_number_of_sets_of_dice_rolls
+		self.attribute_modifier_average/=self.the_number_of_sets_of_dice_rolls
+		if self.attribute_average < self.minimum_average_attribute_or_else_reroll:return dice.statRolls()
+		elif self.attribute_modifier_average < self.minimum_average_attribute_modifier_or_else_reroll:return dice.statRolls()
+		else:
+			self.batch+=1
+			print("batch attempt:"+str(self.batch)+"\nStat Rolls with Modifiers")
+			for batchSets in range(len(self.list_of_roll_set_results)):
+				print("%s  %s |%s" %((self.list_of_roll_set_results[batchSets]), str(self.attribute_results[batchSets]).rjust(2), str(self.attribute_results_modifier[batchSets]).rjust(3)))
+			print("Average Attribute Minimum %.2f/%s\nAverage Modifier Minimum %.2f/%s"%(self.attribute_average, str(self.minimum_average_attribute_or_else_reroll).rjust(2), self.attribute_modifier_average, str(self.minimum_average_attribute_modifier_or_else_reroll).rjust(2)))
+			return
 
 	def displayStatResults(self):
 		print("batch attempt:"+str(self.batch)+"\nStat Rolls with Modifiers")
-		for batch_result_Item in range(len(self.list_of_roll_set_results)):print("%s | %s" %(str(self.attribute_results[batch_result_Item]).rjust(2), str(self.attribute_results_modifier[batch_result_Item]).rjust(3)))
+		for batch_result_Item in range(len(self.list_of_roll_set_results)):print("%s  %s |%s" %((self.list_of_roll_set_results[batch_result_Item]),str(self.attribute_results[batch_result_Item]).rjust(2), str(self.attribute_results_modifier[batch_result_Item]).rjust(3)))
+		print("Average Attribute Minimum %.2f/%s\nAverage Modifier Minimum %.2f/%s"%(self.attribute_average, str(self.minimum_average_attribute_or_else_reroll).rjust(2), self.attribute_modifier_average, str(self.minimum_average_attribute_modifier_or_else_reroll).rjust(2)))
 		return
 
-dice=attributeRolls(the_lowest_possible_roll, the_number_of_sides_on_a_die, the_number_of_rolls_in_a_set, the_number_of_sets_of_dice_rolls, reroll_if_equal_or_less, number_of_lowest_rolls_to_drop_in_a_set, number_of_highest_rolls_to_drop_in_a_set)
+	def displayCurrent(self):
+		print("batch attempt:"+str(self.batch)+"\nStat Rolls with Modifiers")
+		for batch_result_Item in range(len(self.list_of_roll_set_results)):print("%s |%s" %(str(self.attribute_results[batch_result_Item]).rjust(2), str(self.attribute_results_modifier[batch_result_Item]).rjust(3)))
+		return
+
+dice=attributeRolls(the_lowest_possible_roll, the_number_of_sides_on_a_die, the_number_of_rolls_in_a_set, the_number_of_sets_of_dice_rolls, reroll_if_equal_or_less, number_of_lowest_rolls_to_drop_in_a_set, number_of_highest_rolls_to_drop_in_a_set, minimum_average_attribute_or_else_reroll, minimum_average_attribute_modifier_or_else_reroll)
 
 
 '''needs equation to produce modifiers, check for divite by 2, cant just subtract by 10'''
@@ -129,3 +142,8 @@ dice=attributeRolls(the_lowest_possible_roll, the_number_of_sides_on_a_die, the_
 #reworked the set roll code and added in modifiers
 #changed the way Stat Results are displayed
 #still need to rework the character generation menu to have a dice rolls sub menu with roll action, previous batch result list, current batch result list, toggle for attribut result and modifier result averages
+
+"""Update 03-31-16"""
+#Added option for minimum possible Attribute average
+#Added option for minimum possible modifier average
+#realized i had wasted space with shit code in the attribute modifier
